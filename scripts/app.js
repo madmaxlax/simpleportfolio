b38af46ab28f3970293f894c50799ab6f988184c
Max Struever <MaxStruever@gmail.COM>
2018-03-21 15:44:43 +0100

    main functionality added

--- scripts/app.js
+++ scripts/app.js
@@ -5,27 +5,48 @@
 //   return Object.keys(this).length;
 // };
 
+///Set up Angular App
 (function () {
     var app = angular.module('myapp', ['ngResource', 'ngMaterial']);
+    //Set up the main controller
     angular.module('myapp').config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('default')
-          .primaryPalette('indigo')
-          .accentPalette('lime');
-      });
-    angular.module('myapp').controller('appController', ['$scope', '$http', '$resource', '$filter', '$mdSidenav', '$mdDialog', '$mdToast', function ($scope, $http, $resource, $filter, $mdSidenav, $mdDialog, $mdToast) 
-    {
+            .primaryPalette('indigo')
+            .accentPalette('lime');
+    });
+    angular.module('myapp').controller('appController', ['$scope', '$http', '$resource', '$filter', '$mdSidenav', '$mdDialog', '$mdToast', function ($scope, $http, $resource, $filter, $mdSidenav, $mdDialog, $mdToast) {
+        //side nav toggle button
         $scope.toggleLeft = function () {
             $mdSidenav('left').toggle();
         };
 
+        //helper function to get the item in an array with the key Name and the value as given name (if it has one)
+        $scope.getNameFromArray = function(arr, nm) {
+            if(arr instanceof Array && nm != null){
+                for (var i = 0, length = arr.length; i < length; i++) {
+                    if(arr[i].name && arr[i].name === nm)
+                        return arr[i];
+                }
+            }
+            else return null;//return empty object for invalid input
+        };
+
+
         $scope.portfolio = {};
-        $http.get('https://trello.com/b/bgLwimyQ/maxs-web-portfolio.json').then(function(trelloReponse){
-            console.log(trelloReponse);
+        //fetch the portfolio data from Trello
+        $http.get('https://trello.com/b/bgLwimyQ/maxs-web-portfolio.json').then(function (trelloReponse) {
+            console.log(trelloReponse.data);
+
+            //set up the portfolio var
             $scope.portfolio.tags = trelloReponse.data.labels;
             $scope.portfolio.categories = trelloReponse.data.lists;
-            $scope.portfolio.websites = trelloReponse.data.cards;
-        }, function(httpResp){
+            $scope.portfolio.items = trelloReponse.data.cards;
+            // $scope.portfolio.items.forEach(function(portfolioItem){
+            //     console.log($scope.getNameFromArray(portfolioItem.attachments,'Link'));
 
+            // });
+        }, function (httpResp) {
+            alert("Error loading portfolio from Trello!");
         });
     }]);
 })();
---
+++ starthttp.bat
@@ -0,0 +1,3 @@
+cd C:\Apps\GitHub\simpleportfolio
+http-server -p 80
+start chrome http://127.0.0.1
