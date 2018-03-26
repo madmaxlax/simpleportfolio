d400da7309491783e31799efd9a91b5fd2a2b85d
Max Struever <MaxStruever@gmail.COM>
2018-03-26 20:11:37 +0200

    next/prev imagess

--- scripts/app.js
+++ scripts/app.js
@@ -23,28 +23,73 @@
         //initialize filter text
         $scope.portfolioSearchText = '';
 
-        $scope.closeDialog = function(){
-            $mdDialog.hide();
+        //function for when X is clicked to close the portfolio details popup
+        $scope.closeDialog = function () {
             $scope.portfolioDetail = {};
-        }
+            $mdDialog.hide();
+        };
 
-        $scope.showPortfolioDetail = function(portfolioItem){
+        //functions for next and prev image chevrons
+        $scope.setPortfolioDetailImage = function (imageIndex) {
+            $scope.portfolioDetail.currentImage = null;
+            if ($scope.portfolioDetail.attachments[imageIndex].previews.length >= 1) {
+                $scope.portfolioDetail.currentImage = $scope.portfolioDetail.attachments[imageIndex];
+                $scope.setPortfolioDetailNextImage(imageIndex + 1);
+                $scope.setPortfolioDetailPrevImage(imageIndex - 1);
+            }
+            else {
+                //try next one
+                $scope.setPortfolioDetailImage(imageIndex +1);
+            }
+        };
+        $scope.setPortfolioDetailNextImage = function (imageIndex) {
+            $scope.portfolioDetail.nextImageIndex = false;
+            if ($scope.portfolioDetail.attachments[imageIndex] === void 0) {
+                return; //no more images
+            }
+            if ($scope.portfolioDetail.attachments[imageIndex] !== void 0 && $scope.portfolioDetail.attachments[imageIndex].previews.length === 0) {
+                //try next one
+                $scope.setPortfolioDetailNextImage(imageIndex + 1);
+                return;
+            }
+            if ($scope.portfolioDetail.attachments[imageIndex] !== void 0 && $scope.portfolioDetail.attachments[imageIndex].previews.length >= 1) {
+                $scope.portfolioDetail.nextImageIndex = imageIndex;
+            }
+        };
+
+        $scope.setPortfolioDetailPrevImage = function (imageIndex) {
+            $scope.portfolioDetail.prevImageIndex = false;
+            if ($scope.portfolioDetail.attachments[imageIndex] === void 0) {
+                return; //no more images
+            }
+            if ($scope.portfolioDetail.attachments[imageIndex] !== void 0 && $scope.portfolioDetail.attachments[imageIndex].previews.length === 0) {
+                //try Prev one
+                $scope.setPortfolioDetailPrevImage(imageIndex - 1);
+                return;
+            }
+            if ($scope.portfolioDetail.attachments[imageIndex] !== void 0 && $scope.portfolioDetail.attachments[imageIndex].previews.length >= 1) {
+                $scope.portfolioDetail.prevImageIndex = imageIndex;
+            }
+        };
+
+        $scope.showPortfolioDetail = function (portfolioItem) {
             $scope.portfolioDetail = portfolioItem;
             var md = markdownit();
             $scope.portfolioDetail.htmlDesc = md.render($scope.portfolioDetail.desc);
+            $scope.setPortfolioDetailImage(0);
             $mdDialog.show({
                 contentElement: '#portfolioItemDialog',
                 parent: angular.element(document.body),
                 // targetEvent: ev,
                 clickOutsideToClose: true
-              });
+            });
         };
 
         //helper function to get the item in an array with the key Name and the value as given name (if it has one)
-        $scope.getNameFromArray = function(arr, nm) {
-            if(arr instanceof Array && nm != null){
+        $scope.getNameFromArray = function (arr, nm) {
+            if (arr instanceof Array && nm != null) {
                 for (var i = 0, length = arr.length; i < length; i++) {
-                    if(arr[i].name && arr[i].name === nm)
+                    if (arr[i].name && arr[i].name === nm)
                         return arr[i];
                 }
             }
